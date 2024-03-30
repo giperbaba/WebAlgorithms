4f6bd1e86663226a53cb24b2db7ed151e6dd6192
giperbaba <aue64@list.ru>
2024-03-30 18:58:12 +0700

    Add function for update and for click buttons.

--- astarAlgorithm/astar.js
+++ astarAlgorithm/astar.js
@@ -7,6 +7,28 @@ let canvasWidthNum = parseInt(canvasWidth.replace("px", ""));
 canvas.width = canvasWidthNum;
 canvas.height = canvas.width;
 
+let matrix;
+
+let start = document.getElementById('start');
+let finish = document.getElementById('finish');
+let erase = document.getElementById('erase');
+let wall = document.getElementById('wall');
+let begin = document.getElementById('begin');
+
+const paddingJS = 3;
+let columns = document.getElementById("sizeM").value;
+let rows = columns;
+let cellSize = (canvas.width - paddingJS * 2) / columns;
+
+let startCellColor = "#5d001E";
+let finishCellColor = "#EE4C7C";
+let mazeColor = "rosybrown";
+
+let startClicker = false;
+let finishClicker = false;
+let eraseClicker = false;
+let wallClicker = false;
+
 class Cell {
   constructor(x, y) {
     this.x = x;
@@ -14,11 +36,8 @@ class Cell {
   }
 }
 
-const paddingJS = 5;
-let columns = 15;
-let rows = columns;
-let cellSize = (canvas.width - paddingJS * 2) / columns;
-
+let startCell = new Cell(null, null);
+let finishCell = new Cell(null, null);
 
 function drawCell(x, y, color) {
   context.beginPath();
@@ -35,24 +54,31 @@ function drawCell(x, y, color) {
 function createMatrix(columns, rows) {
   let matrix = new Array(columns);
 
-  for (let y = 0; y < rows; y++) { //строки по OY
+  for (let y = 0; y < rows; y++) {
     let column = new Array(rows);
     for (let x = 0; x < columns; x++) {
-      column[x] = false; //false - стена
+      column[x] = false;
     }
     matrix[y] = column;
   }
   return matrix;
 }
 
-function drawMaze(matrix) {
+function drawMaze() {
+  if (startCell.x !== null) {
+    drawCell(startCell.x, startCell.y, startCellColor);
+  }
+  if (finishCell.x !== null) {
+    drawCell(finishCell.x, finishCell.y, finishCellColor);
+  }
+
   context.beginPath();
   context.rect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "black";
+  context.fillStyle = mazeColor;
   context.fill();
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < columns; x++) {
-      const color = (matrix[y][x]) ? "white" : "black";
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix.length; x++) {
+      const color = (matrix[y][x]) ? "white" : mazeColor;
       drawCell(x, y, color);
     }
   }
@@ -109,8 +135,8 @@ function getMazeMatrix(matrix) {
 }
 
 function isValidMaze(matrix) {
-  for (let y = 0; y < rows; y += 2) {
-    for (let x = 0; x < columns; x += 2) {
+  for (let y = 0; y < matrix.length; y += 2) {
+    for (let x = 0; x < matrix.length; x += 2) {
       if (!matrix[y][x]) {
         return false;
       }
@@ -119,10 +145,134 @@ function isValidMaze(matrix) {
   return true;
 }
 
-let matrix = createMatrix(columns, rows);
-while (!isValidMaze(matrix)){
-  matrix = getMazeMatrix(matrix);
+function generateMaze() {
+  context.rect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = mazeColor;
+  context.fill();
+
+  columns = document.getElementById("sizeM").value;
+  matrix = createMatrix(columns, columns);
+
+  while (!isValidMaze(matrix)) {
+    matrix = getMazeMatrix(matrix);
+  }
+  drawMaze();
+  requestAnimationFrame(clickButton);
 }
 
-drawMaze(matrix);
+function main(){
+  while (!isValidMaze(matrix)) {
+    matrix = getMazeMatrix(matrix);
+    requestAnimationFrame(clickButton);
+  }
+  drawMaze(matrix);
+}
 
+function refresh() {
+  columns = document.getElementById("sizeM").value;
+  matrix = createMatrix(columns, columns);
+  cellSize = (canvas.width - paddingJS * 2) / columns;
+
+  startCell = new Cell(null, null);
+  finishCell = new Cell(null, null);
+
+  startClicker = false;
+  finishClicker = false;
+  eraseClicker = false;
+  wallClicker = false;
+
+  main();
+}
+
+function clickButton() {
+
+  start.addEventListener('click', function () {
+    startClicker = true;
+    finishClicker = false;
+    eraseClicker = false;
+    wallClicker = false;
+
+    canvas.addEventListener('mousedown', function (e) {
+      if (startClicker && !finishClicker && !eraseClicker && !wallClicker) {
+        let coordinateX = e.pageX - this.offsetLeft;
+        let coordinateY = e.pageY - this.offsetTop;
+
+        let x = Math.trunc(coordinateX / cellSize);
+        let y = Math.trunc(coordinateY / cellSize);
+
+        if (matrix[y][x] && (finishCell.x !== x || finishCell.y !== y)) {
+          startCell.x = x;
+          startCell.y = y;
+          drawCell(startCell.x, startCell.y, startCellColor);
+        }
+      }
+    });
+  });
+
+  finish.addEventListener('click', function () {
+    startClicker = false;
+    finishClicker = true;
+    eraseClicker = false;
+    wallClicker = false;
+
+    canvas.addEventListener('mousedown', function (e) {
+      if (!startClicker && finishClicker && !eraseClicker && !wallClicker) {
+        let coordinateX = e.pageX - this.offsetLeft;
+        let coordinateY = e.pageY - this.offsetTop;
+
+        let x = Math.trunc(coordinateX / cellSize);
+        let y = Math.trunc(coordinateY / cellSize);
+
+        if (matrix[y][x] && (startCell.x !== x || startCell.y !== y)) {
+          finishCell.x = x;
+          finishCell.y = y;
+          drawCell(finishCell.x, finishCell.y, finishCellColor);
+        }
+      }
+    });
+  });
+
+  erase.addEventListener('click', function () {
+    startClicker = false;
+    finishClicker = false;
+    eraseClicker = true;
+    wallClicker = false;
+
+    canvas.addEventListener('mousedown', function (e) {
+      if (!startClicker && !finishClicker && eraseClicker && !wallClicker) {
+        let coordinateX = e.pageX - this.offsetLeft;
+        let coordinateY = e.pageY - this.offsetTop;
+
+        let x = Math.trunc(coordinateX / cellSize);
+        let y = Math.trunc(coordinateY / cellSize);
+
+        if (!matrix[y][x]) {
+          matrix[x][y] = true;
+          drawCell(x, y, "white");
+        }
+      }
+    });
+  });
+
+  wall.addEventListener('click', function () {
+    startClicker = false;
+    finishClicker = false;
+    eraseClicker = false;
+    wallClicker = true;
+
+    canvas.addEventListener('mousedown', function (e) {
+      if (!startClicker && !finishClicker && !eraseClicker && wallClicker) {
+        let coordinateX = e.pageX - this.offsetLeft;
+        let coordinateY = e.pageY - this.offsetTop;
+
+        let x = Math.trunc(coordinateX / cellSize);
+        let y = Math.trunc(coordinateY / cellSize);
+
+        if (matrix[y][x]) {
+          matrix[x][y] = false;
+          drawCell(x, y, mazeColor);
+        }
+      }
+    });
+  });
+}
