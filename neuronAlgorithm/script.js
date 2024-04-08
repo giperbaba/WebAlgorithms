bc9df63933ab5907d8c73a8df9ad74f626011dce
sslonixxx <gergetsofya2005@gmail.com>
2024-04-08 12:34:03 +0700

    added read csv files

--- neuronAlgorithm/script.js
+++ neuronAlgorithm/script.js
@@ -1,8 +1,24 @@
-
+async function fetchDataFromCSV(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Ошибка загрузки файла');
+    }
+    const csvText = await response.text();
+    const csvArray = csvText.split('\n').map(line => line.split(','));
+    return csvArray;
+  } catch (error) {
+    console.error('Ошибка:', error);
+    return null;
+  }
+}
 class NeuralNetwork {
     constructor() {
-        // this.biases;
-        // this.weights;
+        this.biases1=fetchDataFromCSV('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/biases1.csv');
+        this.biases2=fetchDataFromCSV('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/biases2.csv');
+        this.weights1=fetchDataFromCSV('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/weight1.csv');
+        this.weights2=fetchDataFromCSV('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/weight2.csv');
+        console.log(this.biases1);
       //импортирование весов из файла после обучения
     }
 
