7a960bdf97dbe4f22a1aeb929411ab561ecc828f
sslonixxx <gergetsofya2005@gmail.com>
2024-04-09 22:40:06 +0700

    added visualization and new weights

--- neuronAlgorithm/draw.js
+++ neuronAlgorithm/draw.js
@@ -9,7 +9,7 @@ canvas.height = 400;
 
 ctx.strokeStyle = 'white';
 ctx.fillStyle = 'black';
-ctx.lineWidth = 24;
+ctx.lineWidth = 26;
 
 // Заполняем канвас пикселями 5x5
 for (let x = 0; x < canvas.width; x += 8) {
@@ -31,6 +31,15 @@ clear.addEventListener('click', e => {
       }
     }
   }
+  const elDigits = Array.from(document.querySelectorAll('.digits-field div'));
+
+  elDigits.forEach(function(digit) {
+    digit.className = 'digit';
+  });
+
+  elDigits.classList.remove('recognized-digit-first');
+  elDigits.classList.remove('recognized-digit-second');
+  elDigits.classList.remove('recognized-digit-third');
 });
 
 function startDrawing(e) {
@@ -55,34 +64,6 @@ canvas.addEventListener('mousemove', drawLine);
 
 const saveButton = document.getElementById('save');
 
-function showPopupMessage(message, duration) {
-  // Создаем элемент для сообщения
-  var popup = document.createElement('div');
-  popup.className = 'popup-message';
-  popup.textContent = message;
-
-  // Стилизуем сообщение
-  popup.style.position = 'fixed';
-  popup.style.top = '50px'; // Расположение сверху
-  popup.style.left = '50%'; // Горизонтальное выравнивание
-  popup.style.transform = 'translateX(-50%)'; // Центрируем по горизонтали
-  popup.style.backgroundColor = '#333';
-  popup.style.color = '#fff';
-  popup.style.padding = '10px';
-  popup.style.borderRadius = '5px';
-  popup.style.zIndex = '9999'; // Устанавливаем высокий z-index, чтобы было поверх других элементов
-
-  // Добавляем сообщение на страницу
-  document.body.appendChild(popup);
-
-  // Удаляем сообщение через указанное время
-  setTimeout(function() {
-    document.body.removeChild(popup);
-  }, duration);
-}
-
-// Пример использования функции
-
 
 saveButton.addEventListener('click', () => {
   let csvContent = "data:text/csv;charset=utf-8,";
@@ -98,7 +79,17 @@ saveButton.addEventListener('click', () => {
   const sortedIndexes = rez.map((value, index) => index)
     .sort((a, b) => rez[b] - rez[a])
     .slice(0, 3);
-  showPopupMessage(sortedIndexes, 30000); 
+  const elDigits = Array.from(document.querySelectorAll('.digits-field div'));
+
+  elDigits.forEach(function(digit) {
+    digit.className = 'digit';
+  });
+
+  elDigits[sortedIndexes[0]].classList.add('recognized-digit-first');
+  elDigits[sortedIndexes[1]].classList.add('recognized-digit-second');
+  elDigits[sortedIndexes[2]].classList.add('recognized-digit-third');
 
   console.log(sortedIndexes);
 });
+
+
