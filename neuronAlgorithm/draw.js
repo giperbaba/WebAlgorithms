e8aeaa7265af6b8c9236f582a6d753ef7383b522
sslonixxx <gergetsofya2005@gmail.com>
2024-04-09 19:24:51 +0700

    added recognize digits with weights

--- neuronAlgorithm/draw.js
+++ neuronAlgorithm/draw.js
@@ -1,67 +1,3 @@
-// const canvas = document.querySelector('canvas');
-// const clear = document.getElementById('clear');
-// const ctx = canvas.getContext('2d');
-//
-// const canvasOffsetX = canvas.offsetLeft;
-// const canvasOffsetY = canvas.offsetTop;
-// canvas.width = 400;
-// canvas.height = 400;
-//
-// ctx.strokeStyle = 'white';
-// ctx.fillStyle = 'black';
-// ctx.lineWidth = 15;
-//
-// // Заполняем канвас пикселями 5x5
-// for (let x = 0; x < canvas.width; x += 5) {
-//   for (let y = 0; y < canvas.height; y += 5) {
-//     ctx.fillRect(x, y, 5, 5);
-//   }
-// }
-//
-// let isDrawing = false;
-//
-// clear.addEventListener('click', e => {
-//   if (e.target.id === 'clear') {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//
-//     // Заполняем канвас пикселями 5x5
-//     for (let x = 0; x < canvas.width; x += 5) {
-//       for (let y = 0; y < canvas.height; y += 5) {
-//         ctx.fillRect(x, y, 5, 5);
-//       }
-//     }
-//   }
-// });
-//
-// function startDrawing(e) {
-//   isDrawing = true;
-//   ctx.beginPath();
-//   ctx.moveTo(Math.floor(e.offsetX / 10) * 10, Math.floor(e.offsetY / 10) * 10);
-// }
-//
-// function stopDrawing() {
-//   isDrawing = false;
-// }
-//
-// function drawLine(e) {
-//   if (!isDrawing) return;
-//   ctx.lineTo(Math.floor(e.offsetX / 10) * 10, Math.floor(e.offsetY / 10) * 10);
-//   ctx.stroke();
-// }
-//
-// canvas.addEventListener('mousedown', startDrawing);
-// canvas.addEventListener('mouseup', stopDrawing);
-// canvas.addEventListener('mousemove', drawLine);
-//
-// const saveButton = document.getElementById('save');
-//
-// saveButton.addEventListener('click', () => {
-//   const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-//   const link = document.createElement('a');
-//   link.href = image;
-//   link.download = 'my_drawing.png';
-//   link.click();
-// });
 const canvas = document.querySelector('canvas');
 const clear = document.getElementById('clear');
 const ctx = canvas.getContext('2d');
@@ -73,7 +9,7 @@ canvas.height = 400;
 
 ctx.strokeStyle = 'white';
 ctx.fillStyle = 'black';
-ctx.lineWidth = 16;
+ctx.lineWidth = 24;
 
 // Заполняем канвас пикселями 5x5
 for (let x = 0; x < canvas.width; x += 8) {
@@ -119,24 +55,50 @@ canvas.addEventListener('mousemove', drawLine);
 
 const saveButton = document.getElementById('save');
 
+function showPopupMessage(message, duration) {
+  // Создаем элемент для сообщения
+  var popup = document.createElement('div');
+  popup.className = 'popup-message';
+  popup.textContent = message;
+
+  // Стилизуем сообщение
+  popup.style.position = 'fixed';
+  popup.style.top = '50px'; // Расположение сверху
+  popup.style.left = '50%'; // Горизонтальное выравнивание
+  popup.style.transform = 'translateX(-50%)'; // Центрируем по горизонтали
+  popup.style.backgroundColor = '#333';
+  popup.style.color = '#fff';
+  popup.style.padding = '10px';
+  popup.style.borderRadius = '5px';
+  popup.style.zIndex = '9999'; // Устанавливаем высокий z-index, чтобы было поверх других элементов
+
+  // Добавляем сообщение на страницу
+  document.body.appendChild(popup);
+
+  // Удаляем сообщение через указанное время
+  setTimeout(function() {
+    document.body.removeChild(popup);
+  }, duration);
+}
+
+// Пример использования функции
+
 
 saveButton.addEventListener('click', () => {
   let csvContent = "data:text/csv;charset=utf-8,";
-  const imageMatrix=[];
+  let imageMatrix=[];
 
-  for (let y = 0; y < canvas.height; y += 16) {
-    for (let x = 0; x < canvas.width; x += 16) {
+  for (let y = 0; y < canvas.height; y += 8) {
+    for (let x = 0; x < canvas.width; x += 8) {
       const pixelData = ctx.getImageData(x, y, 1, 1).data;
-      console.log(pixelData);
-      const color = [pixelData[2]];
-      csvContent += color + ",";
+      imageMatrix.push([pixelData[2]/255]);
     }
-    csvContent += "\n";
   }
+  let rez=net.feedforward(imageMatrix)
+  const sortedIndexes = rez.map((value, index) => index)
+    .sort((a, b) => rez[b] - rez[a])
+    .slice(0, 3);
+  showPopupMessage(sortedIndexes, 30000); 
 
-  const encodedUri = encodeURI(csvContent);
-  const link = document.createElement('a');
-  link.href = encodedUri;
-  link.download = 'pixel_colors.csv';
-  link.click();
+  console.log(sortedIndexes);
 });
--- neuronAlgorithm/script.js
+++ neuronAlgorithm/script.js
@@ -1,51 +1,89 @@
-async function fetchDataFromCSV(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Ошибка загрузки файла');
-    }
-    const csvText = await response.text();
-    const csvArray = csvText.split('\n').map(line => line.split(','));
-    return csvArray;
-  } catch (error) {
-    console.error('Ошибка:', error);
-    return null;
-  }
+
+function readCSVFile(url) {
+  return fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      const rows = data.split('\n');
+      const matrix = rows.map(row => row.split(',').map(parseFloat));
+      matrix.pop()
+      return matrix;
+    });
 }
 class NeuralNetwork {
-    constructor() {
-        this.biases1=fetchDataFromCSV('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/biases1.csv');
-        this.biases2=fetchDataFromCSV('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/biases2.csv');
-        this.weights1=fetchDataFromCSV('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/weight1.csv');
-        this.weights2=fetchDataFromCSV('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/weight2.csv');
-        console.log(this.biases1);
-      //импортирование весов из файла после обучения
-    }
+  constructor() {
+    this.initializeData();
+  }
 
+  async initializeData() {
+    this.biases1 = await readCSVFile('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/biases1.csv');
+    this.biases2 = await readCSVFile('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/biases2.csv');
+    this.weights1 = await readCSVFile('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/weight1.csv');
+    this.weights2 = await readCSVFile('https://raw.githubusercontent.com/giperbaba/WebAlgorithms/neuroAlgorithm/neuronAlgorithm/weight2.csv');
+  }
   sigmoid(x)
   {
-    return 1 / (1 + Math.exp(-x));
+    //console.log(x)
+    for (let i = 0; i < x.length; i++){
+      x[i]= 1 / (1 + Math.exp(-x[i]))}
+    return x
   }
-
-  dotProduct(v1, v2) {
-    let result = 0;
-    for (let i = 0; i < v1.length; i++) {
-      result += v1[i] * v2[i];
+  relu(x){
+    //console.log(x)
+    for (let i = 0; i < x.length; i++){
+      if(x[i]<0){
+        x[i]=[0];
+      }
     }
+    return x;
+  }
+  dotProduct(v1, v2) {
+    let res = [];
+    //console.log(v2[0])
+    for (let i = 0; i < v1.length; i++) {
+      res[i] = [];
+      for (let j = 0; j < v2[0].length; j++) {
+        let sum = 0;
+        for (let k = 0; k < v1[0].length; k++) {
+          //console.log(v1[i][k] , v2[k][j])
+          sum += v1[i][k] * v2[k][j];
+        }
+        res[i][j] = sum;
+      }
+    }
+    return res;
+  }
+  sum(matrix1,matrix2){
+    let result = [];
+    for (let i = 0; i < matrix1.length; i++) {
+      // Проверяем, является ли matrix1 одномерным массивом
+      if (!Array.isArray(matrix1[i])) {
+        result.push(matrix1[i] + matrix2[i][0]);
+      } else {
+        let row = [];
+        for (let j = 0; j < matrix1[i].length; j++) {
+          row.push(matrix1[i][j] + matrix2[i][j]);
+        }
+        result.push(row);
+      }
+    }
+    //console.log(result)
     return result;
   }
-
   feedforward(a) {
     // Return the output of the network if "a" is input.
-    for (let i = 0; i < this.biases.length; i++) {
-      let b = this.biases[i];
-      let w = this.weights[i];
-      a = sigmoid(dotProduct(w, a) + b);
-    }
-    return a;
+    let b1 = this.biases1;
+    let w1 = this.weights1;
+    let b2 = this.biases2;
+    let w2 = this.weights2;
+    let layear1 = this.relu(this.sum(this.dotProduct(w1, a),b1));
+    //console.log(layear1)
+    //console.log(this.dotProduct(w2, layear1))
+    //console.log(this.sum(this.dotProduct(w2, layear1),b2))
+    let output = this.sigmoid(this.sum(this.dotProduct(w2, layear1),b2));
+    //console.log(output)
+    return output;
   }
 
 }
 
 const net = new NeuralNetwork();
-
